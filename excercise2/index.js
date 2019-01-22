5c4c553ca9e6bed41a6c44df13b4a69ad53c30c4
Adam Collins <adamcollins@adamcollins-macbookpro.roam.corp.google.com>
2019-01-22 12:18:09 -0800

    bcm

--- excercise2/index.js
+++ excercise2/index.js
@@ -1,28 +1,42 @@
 let WINNING_SCORE = 3;
 let player1Score = 0;
 let player2Score = 0;
-;
 
+/**
+ * Asks the first player for their name.
+ */
 function askForPlayer1Name() {
   return prompt("Player 1, what is your name?")
 }
 
+/**
+ * Alert the user that they made an illegal move.
+ */
 function illegalMove(name, move) {
   alert(name + ", " + move + " is an illegal move minus 1!");
 }
 
+/**
+ * Display a message for when there's a tie
+ */
 function tieMessage() {
   // TODO
 }
 
-function displayMessage(/** Add args **/) {
+/**
+ * Log the score in console
+ */
+function displayGameStatus(/** Add args **/) {
   // TODO
 }
 
+/**
+ * Performs all actions required to play a game of Rock, Paper, Scissors.
+ */
 function play(player1Name, player2Name) {
   let player1Move = prompt(player1Name + ", rock, paper, or scissors?")
   let player2Move = prompt(player2Name + ", rock, paper, or scissors?")
-  
+
   if(player1Move === null || player2Move === null) {
     return;
   }
@@ -31,5 +45,5 @@ function play(player1Name, player2Name) {
 }
 
 let player1Name = askForPlayer1Names();
-let player2Name = askForPlayer2Names(); // TODO Write this method
+let player2Name = askForPlayer2Names(); // TODO Write this function
 play(player1Name, player2Name);
